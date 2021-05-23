7c4a2ab3ca9c878a474b798fcd59d2399870b6d3
Derrick Lee <derricklee@accredify.io>
2021-05-23 20:03:34 +0800

    Added PriceHelperTest and PriceHelper Class

---
+++ app/Classes/PriceHelper.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Classes;
+
+class PriceHelper
+{
+    /*
+     * Todo: Coding Test for Technical Hires
+     * Please read the instructions on the README.md
+     * Your task is to write the functions for the PriceHelper class
+     * A set of sample test cases and expected results can be found in PriceHelperTest
+     */
+
+    /**
+     * Task: Write a function to return the unit price of an item based on the quantity
+     *
+     * Question:
+     * If I purchase 10,000 bicycles, the unit price of the 10,000th bicycle would be 1.50
+     * If I purchase 10,001 bicycles, the unit price of the 10,001st bicycle would be 1.00
+     * If I purchase 100,001 bicycles, what would be the unit price of the 100,001st bicycle?
+     *
+     * @param int $qty
+     * @param array $tiers
+     * @return float
+     */
+    public static function getUnitPriceTierAtQty(int $qty, array $tiers): float
+    {
+        return 0.0;
+    }
+
+    /**
+     * Task: Write a function to return the total price based on the quantity
+     *
+     * Question:
+     * If I purchase 10,000 bicycles, the total price would be 1.5 * 10,000 = $15,000
+     * If I purchase 10,001 bicycles, the total price would be (1.5 * 10,000) + (1 * 2) = $15,001
+     * If I purchase 100,001 bicycles, what would the total price be?
+     *
+     * @param int $qty
+     * @param array $tiers
+     * @return float
+     */
+    public static function getTotalPriceTierAtQty(int $qty, array $tiers): float
+    {
+        return 0.0;
+    }
+
+    /**
+     * Task: Write a function to return an array of prices at each quantity
+     *
+     * Question A:
+     * A user purchased 933, 22012, 24791 and 15553 bicycles respectively in Jan, Feb, Mar, April
+     * The management would like to know how much to bill this user for each of those month.
+     * This user is on a special pricing tier where the quantity does not reset each month and is thus CUMULATIVE.
+     *
+     * Question B:
+     * A user purchased 933, 22012, 24791 and 15553 bicycles respectively in Jan, Feb, Mar, April
+     * The management would like to know how much to bill this user for each of those month.
+     * This user is on the typical pricing tier where the quantity RESETS each month and is thus NOT CUMULATIVE.
+     *
+     */
+    public static function getPriceAtEachQty(array $qtyArr, array $tiers, bool $cumulative = false): array
+    {
+       return [];
+    }
+}
