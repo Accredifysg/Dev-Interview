68d297de5ce3d8aabef15570854fcdf4f70ca6d2
Abdul Hakim <abdul.hakim@accredify.com>
2021-11-11 18:20:58 +0800

    PAVE-92: add front end dir

---
+++ front-end/boilerplate/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloI18n.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
