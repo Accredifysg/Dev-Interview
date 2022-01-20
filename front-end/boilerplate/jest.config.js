35a8b5e3dfc68fe745c416b9da7ba909287c7637
Abdul Hakim <34259098+Abdulhakimsg@users.noreply.github.com>
2022-01-20 16:33:08 +0800

    PAVE-00: update jest config

--- front-end/boilerplate/jest.config.js
+++ front-end/boilerplate/jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/i18n.js',
+    '!src/main.js',
+  ],
   transform: {
     '^.+\\.vue$': 'vue-jest',
   },
